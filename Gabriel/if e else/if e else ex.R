cdea748addb296616cc4e02048a6bc5909b74df0
rodriguesgabriel1 <gaabrielrodrigues12@gmail.com>
2023-10-02 09:19:37 -0300

    exercício de if e else

---
+++ Gabriel/if e else/if e else ex.R
@@ -0,0 +1,18 @@
+## apredendo a utilizar as estruuturas condicionais if e else ##
+
+#exemplo 1: utilizando if e else separados 
+# Defina um número
+numero <- 8
+
+# Verifique se o número é maior que 10
+if (numero > 10) {
+  print("O número é maior que 10.")
+} else {
+  print("O número é menor ou igual a 10.")
+}
+
+#exemplo 2: utilizando ifselse juntos
+
+idade <- 23
+ifelse(idade>18, "maior idade",  "menor idade")
+  
