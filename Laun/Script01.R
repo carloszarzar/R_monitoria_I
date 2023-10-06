3e27cd1c107a027f0032271634623594c02490e1
LuanPatck <luan.souza@discente.ufopa.edu.br>
2023-10-06 15:25:26 -0300

    Apresentação de monitoria do dia 03/10/23

--- Laun/Script01.R
+++ Laun/Script01.R
@@ -1,55 +1,100 @@
-#######################################
-##--------------Aula 01---------------#
-# Scripts do R Basico                 #
-# Autores: Luan Patrick               #
-#######################################
-# CODIGO Script01
-#######################################
+------------------------------------
+# Script 01 Monitoria R
+# Autor: Luan Patrick
+# Data: 03 / 10 / 2023
 
-# Operações matemáticas 
-5+5
-5-5
-5/5
-5*5
+-------------------------------
+# Exemplos de Soma 
+20 + 1
+30 + 54
+45 + 5
 
-# Exemplo de atribuição de variáveis
+# Exemplos de Subtração
+20 - 12
+30 - 54
+45 - 7
 
-x <- 5
-y <- 10
-z <- 25
-nome <- "João"
-mensagem <- "Olá, mundo!"
+# Exemplos de Multiplicação
+20 * 1
+45 * 54
+45 * 14
 
-----------------------------------------------
-# Exluindo uma variável
-rm()
+#Exemplos de Divisão
+4 / 2
+-2 / 7
+5 / 5
+#Exemplos Exponenciação
+114 ^ 14
+-5 ^ 5
+ 25 ^ 25
+ 
+# Exemplos EDivisão por inteiro
+7 %% 3
+7 %/% 3
 
-----------------------------------------------
-  
-print(x)  # Isso imprimirá o valor de x no console.
+#Atribuindo valores
+a <- 10
+b <- 4
 
-print(mensagem)  # Isso imprimirá a mensagem no console.
+# Operções com variáveis
+a * b
+a ^ b
+a / b
 
-print(x, y, z)  # Isso imprimirá os valores de x, y e z na mesma linha.
+# criando vetores 
+x <- c(2,4,6)
+y <- c(10,8,6)
 
------------------------------------------------
+#operaçõe com vetore
 
-# Exemplo de criação de vetores
-
-numeros <- c(1, 2, 3, 4, 5)
-nomes <- c("Alice", "Bob", "Charlie")
+x + y
 
 
-ls(nomes)
-amostra <- sample(1:10, 10)
+# Função da Média
+mean(x)
+mean(y)
 
-amostra
+var(x)
+var(y)
 
-which(amostra == 1)
+sum(x)
 
-which.max(amostra)
-which.min(amostra)
-rm(amostra)
-which.max(amostra)
+#Criando Sequência 
+2:200
+z <- c(1:100)
+z
+rm(a)
+rm(b
+   )
+rm(x)
+rm(y)
+rm(z)
 
-which(amostra == max(amostra))
+
+N <- 100
+x <- 1:N
+y <- 5 + 2 * x + rnorm(N,sd=30)
+y
+
+plot(x,y)
+
+plot(x,y,main = 'Gráfico de Dispersão',xlab = 'Eixo X', ylab = 'Eixo Y', pch = 16, col = "blue")
+
+categorias <-c("A","B","C","D")
+valores <- c(25, 40, 15, 30)
+
+barplot(valores,names.arg = categorias, main = "Gráfico de Barras", xlab="Categorias", ylab = "valores", col = "green")
+
+
+dados <- rnorm(1000)
+dados
+
+hist(dados, main = "Histograma", xlab = "Valores", ylab = "Frequência", col = "purple")
+
+matriz <- matrix(data = 1:10, ncol = 2, nrow = 5)
+matriz
+str(matriz)
+
+nrow(matriz)
+ncol(matriz)
+length(matriz)
---
+++ Laun/Script02.R
@@ -0,0 +1,39 @@
+------------------------------------
+# Script 02 Monitoria R
+# Autor: Luan Patrick
+# Data: 03 / 10 / 2023
+
+# Criando uma Matriz
+
+dados <-c(1:25)
+           
+matriz <- matrix (data = dados, nrow = 5, ncol = 5)
+
+
+matriz
+
+str(matriz)
+rm (matriz)
+# Numero de Linhas
+nrow(matriz)
+
+# Nímero de Colunas
+ncol(matriz)
+
+#Tamanho da matriz
+length(matriz)
+
+#Inversão da matriz
+solve(matriz)
+
+
+# Verificando se um número é positivo, negativo ou zero
+x <- -5
+
+if (x > 0) {
+  print("x é positivo")
+} else if (x < 0) {
+  print("x é negativo")
+} else {
+  print("x é zero")
+}
